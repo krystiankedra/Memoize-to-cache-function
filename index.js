a021fd2a3122ae5e45c90653d3cfe9e0384f6e50
Kedra, Krystian <krystian.kedra@atos.net>
2019-12-08 20:59:07 +0100

    Added more example

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 const memoizedAdd = () => {
   const cache = {}
-  return (a, b) => {
-    if (`${a}+${b}` in cache) {
-      return cache[`${n}+${m}`]
+  return (n) => {
+    if (n in cache) {
+      return cache[n]
     } else {
-      const result = a + b + 10
-      cache[`${a}+${b}`] = result
+      const result = n + 10
+      cache[n] = result
       return result
     }
   }
 }
 const newAdd = memoizedAdd()
-const result1 = newAdd(9, 10)
-console.log(result1)
-const result2 = newAdd(9, 10)
-console.log(result2)
+const result1 = newAdd(9)
+const result2 = newAdd(9)
+
+
+const positiveObject = {
+  status: 'Activated',
+  result: 5 + 2 + 10
+}
+
+const negativeObject = {
+  status: 'Deactivated',
+  result: 20 + 10 + 70
+}
+
+const memoizedObjectByTyoe = () => {
+  const cache = {}
+  return (obj, n) => {
+    const { status, result } = obj
+    const key = `${status}${n}`
+    if (key in cache) {
+      return cache[key]
+    } else {
+      const newResult = result * n
+      cache[key] = newResult
+      return newResult
+    }
+  }
+}
+
+const objectByType = memoizedObjectByTyoe()
+const resultO1 = objectByType(positiveObject, 5)
+const resultO2 = objectByType(positiveObject, 5)
+const resultO3 = objectByType(negativeObject, 5)
+const resultO4 = objectByType(negativeObject, 5)
+const resultO5 = objectByType(positiveObject, 2)
